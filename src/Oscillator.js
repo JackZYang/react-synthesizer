08f1b8f9b0025fca3e6e0ef460d1636a556f465f
Jack <jackzhuoyang@gmail.com>
2019-10-04 18:08:48 +1000

    Added different waveforms

--- src/Oscillator.js
+++ src/Oscillator.js
@@ -1,22 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 class Oscillator extends Component {
-    
-    playSound(){
-        var oscillator = audioContext.createOscillator();
-        oscillator.connect(audioContext.destination);
-        oscillator.start();
-    }
+  state = {
+    waveform: "square",
+    playing: false
+  };
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.playSound}>play</button>
-            </div>
-        )
+  changeWaveform = e => {
+    const id = e.target.id;
+    switch (id) {
+      case "sin":
+        this.setState({
+          waveform: "sin"
+        });
+        break;
+      case "square":
+        this.setState({
+          waveform: "square"
+        });
+        break;
+      case "triangle":
+        this.setState({
+          waveform: "triangle"
+        });
+        break;
+      case "sawtooth":
+        this.setState({
+          waveform: "sawtooth"
+        });
+        break;
     }
+  };
+
+  playSound = () => {
+    var oscillator = audioContext.createOscillator();
+    oscillator.connect(audioContext.destination);
+    oscillator.type = this.state.waveform;
+    oscillator.start();
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.playSound}>play</button>
+        <button onClick={this.changeWaveform} id="sin">
+          Sin
+        </button>
+        <button onClick={this.changeWaveform} id="square">
+          Square
+        </button>
+        <button onClick={this.changeWaveform} id="triangle">
+          Triangle
+        </button>
+        <button onClick={this.changeWaveform} id="sawtooth">
+          Sawtooth
+        </button>
+        {this.state.waveform}
+      </div>
+    );
+  }
 }
 
-export default Oscillator
+export default Oscillator;
