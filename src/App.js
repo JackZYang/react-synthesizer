389adb9bc407b6cec6ebccbf7c8df370c7e4a9e7
Jack <jackzhuoyang@gmail.com>
2019-10-11 17:01:51 +1100

    Readd waveform changer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,49 @@ import Oscillator from "./Oscillator.js";
 import PianoRoll from "./PianoRoll.js";
 
 class App extends Component {
+  state = {
+    waveform: "sine"
+  };
+
+  changeWaveform = e => {
+    switch (e.target.value) {
+      case "sine":
+        this.setState({
+          waveform: "sine"
+        });
+        break;
+      case "square":
+        this.setState({
+          waveform: "square"
+        });
+        break;
+      case "triangle":
+        this.setState({
+          waveform: "triangle"
+        });
+        break;
+      case "sawtooth":
+        this.setState({
+          waveform: "sawtooth"
+        });
+        break;
+    }
+  };
+
   render() {
     return (
       <div className="App">
+        <select onChange={this.changeWaveform}>
+          <option value="sine">Sine</option>
+          <option value="square">Square</option>
+          <option value="triangle">Triangle</option>
+          <option value="sawtooth">Sawtooth</option>
+        </select>
         <Oscillator
-          render={(playSound, stopSound, oscillators) => (
-            <PianoRoll
-              playSound={playSound}
-              stopSound={stopSound}
-              oscillators={oscillators}
-            />
+          render={(playSound, stopSound) => (
+            <PianoRoll playSound={playSound} stopSound={stopSound} />
           )}
+          waveform={this.state.waveform}
         />
       </div>
     );
--- src/Oscillator.js
+++ src/Oscillator.js
@@ -5,62 +5,30 @@ let audioContext = new AudioContext(),
 
 class Oscillator extends Component {
   state = {
-    waveform: "sine",
     oscillators: {}
-  };
-
-  changeWaveform = e => {
-    const id = e.target.id;
-    switch (id) {
-      case "sine":
-        this.setState({
-          waveform: "sine"
-        });
-        break;
-      case "square":
-        this.setState({
-          waveform: "square"
-        });
-        break;
-      case "triangle":
-        this.setState({
-          waveform: "triangle"
-        });
-        break;
-      case "sawtooth":
-        this.setState({
-          waveform: "sawtooth"
-        });
-        break;
-    }
   };
 
   playSound = midiNumber => {
     oscillator = audioContext.createOscillator();
     oscillator.connect(audioContext.destination);
-    oscillator.type = this.state.waveform;
+    oscillator.type = this.props.waveform;
     oscillator.frequency.value = Math.pow(2, (midiNumber - 69) / 12) * 440;
-    this.state.oscillators[midiNumber] = oscillator;
-    console.log(this.state.oscillators);
+    this.setState({
+      oscillators: {
+        ...this.state.oscillators,
+        [midiNumber]: oscillator
+      }
+    });
     oscillator.start();
   };
 
   stopSound = midiNumber => {
     this.state.oscillators[midiNumber].stop();
     delete this.state.oscillators[midiNumber];
-    console.log(this.state.oscillators);
   };
 
   render() {
-    return (
-      <div>
-        {this.props.render(
-          this.playSound,
-          this.stopSound,
-          this.state.oscillators
-        )}
-      </div>
-    );
+    return <div>{this.props.render(this.playSound, this.stopSound)}</div>;
   }
 }
 
